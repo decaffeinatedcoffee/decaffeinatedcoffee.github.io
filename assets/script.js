23d255b278f0de909044a22c4a9ca0fc1047cb98
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2021-10-14 13:12:52 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -33,6 +33,7 @@ function turnenglish (){
     document.getElementById("tabuada").innerHTML = "Multiplication table";
     document.getElementById("previsaotempo").innerHTML = "Weather forecast";
     document.getElementById("verify").innerHTML = "Verify";
+    document.getElementById("CEP").innerHTML = "CEP ( Only in Brazil ): )";
     document.getElementById("listaidiomas").style.display = "none";
     document.getElementById("portugues").style.display = "none";
     document.getElementById("english").style.display = "none";
@@ -49,6 +50,7 @@ function turnportugues (){
     document.getElementById("tabuada").innerHTML = "Tabuada";
     document.getElementById("previsaotempo").innerHTML = "Previsão do tempo";
     document.getElementById("verify").innerHTML = "Verificação";
+    document.getElementById("CEP").innerHTML = "CEP";
     document.getElementById("listaidiomas").style.display = "none";
     document.getElementById("portugues").style.display = "none";
     document.getElementById("english").style.display = "none";
@@ -65,6 +67,7 @@ function turndeutsch(){
     document.getElementById("tabuada").innerHTML = "Multiplikationstabelle";
     document.getElementById("previsaotempo").innerHTML = "Wettervorhersage";
     document.getElementById("verify").innerHTML = "Prüfen";
+    document.getElementById("CEP").innerHTML = "CEP ( nur in Brasilien ): )";
     document.getElementById("listaidiomas").style.display = "none";
     document.getElementById("portugues").style.display = "none";
     document.getElementById("english").style.display = "none";
