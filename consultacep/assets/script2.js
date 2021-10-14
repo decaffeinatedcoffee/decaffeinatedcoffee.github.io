6def86604c0b359145936d04fbe6ac989199ca7e
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2021-10-14 13:03:59 -0300

    Update script2.js

--- consultacep/assets/script2.js
+++ consultacep/assets/script2.js
@@ -12,17 +12,23 @@ fetch("https://viacep.com.br/ws/"+ UF + "/" + cidade + "/" + logradouro + "/json
   .then(function (myJson) {
     document.getElementById("pre").style.display = 'none'
       document.getElementById("pos").style.display = 'block'
+      
       document.getElementById("resultadoinfo").innerHTML = myJson[0].logradouro + ", " + myJson[0].bairro + ", " + myJson[0].localidade + ", " + myJson[0].uf;
   document.getElementById("resultado").innerHTML = "CEP: " + myJson[0].cep + " (DDD " + myJson[0].ddd + ")";
+ 
 
-
-  });
+  })
+  .catch(function(error) {
+    document.getElementById("feedback2").innerHTML = "Eita, Acho que esse endereço está errado, Você pode tentar novamente?";
+});
 }
+    
 else{
     document.getElementById("feedback").innerHTML = "Não podem existir campos vazios!"
 
 }
 }
+
 function repetir() {
 
 location.reload();
