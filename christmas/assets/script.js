b68d61d3bfb438287347ce9e7ea5e47e3cc425a7
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2021-12-24 14:37:32 -0300

    Update script.js

--- christmas/assets/script.js
+++ christmas/assets/script.js
@@ -32,6 +32,7 @@ let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
 let data = new Date();
 data.toLocaleString('en-US', { timeZone: timezone });
 var horaatual = data.getHours();
+var currentday = data.getDate();
 var atual = new Date().getTime();
 var end = new Date('Dec 25, 2021 00:00:00').getTime();
 var restante = end - atual;
@@ -46,7 +47,7 @@ if(dias <= 0 && horas <= 0 && minutos <= 0 && segundos <= 0){
     document.querySelector("link[rel*='icon']").href = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎅</text></svg>";
     document.title = "Its christmas!"
 }
-else if(dias == 0 && horas <= 23 && minutos <= 59 && segundos >= 1){
+else if(dias == 0 && currentday == 24){
 document.getElementById("resposta").innerHTML = "NO, BUT ALMOST"
 document.getElementById("gift").style.display = "none"
 document.getElementById("timer").innerHTML = dias + " days " +  horas + " hours " + minutos + " minutes and " + segundos + " seconds until Christmas"
