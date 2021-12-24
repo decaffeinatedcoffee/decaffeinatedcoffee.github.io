dff3a73ad616f4fca747fc08cce7b56e567e4d9d
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2021-12-24 14:31:40 -0300

    Update script.js

--- christmas/assets/script.js
+++ christmas/assets/script.js
@@ -33,7 +33,7 @@ let data = new Date();
 data.toLocaleString('en-US', { timeZone: timezone });
 var horaatual = data.getHours();
 var atual = new Date().getTime();
-var end = new Date('Dec 25, 2021 00:00:00').getTime();
+var end = new Date('Dec 28, 2021 00:00:00').getTime();
 var restante = end - atual;
 var dias = Math.floor(restante / (1000 * 60 * 60 * 24));
 var horas = Math.floor((restante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -45,6 +45,14 @@ if(dias <= 0 && horas <= 0 && minutos <= 0 && segundos <= 0){
     document.getElementById("gift").style.display = "block"
     document.querySelector("link[rel*='icon']").href = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎅</text></svg>";
     document.title = "Its christmas!"
+}
+else if(dias == 0 && horas <= 23 && minutos <= 59 && segundos >= 1){
+document.getElementById("resposta").innerHTML = "NO, BUT ALMOST"
+document.getElementById("gift").style.display = "none"
+document.getElementById("timer").innerHTML = dias + " days " +  horas + " hours " + minutos + " minutes and " + segundos + " seconds until Christmas"
+document.querySelector("link[rel*='icon']").href = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>"+ hours[horaatual] +"</text></svg>";
+document.title = "Its not christmas yet 😢"
+setTimeout(timer, 1000);
 }
 else{
 document.getElementById("resposta").innerHTML = "NO"
