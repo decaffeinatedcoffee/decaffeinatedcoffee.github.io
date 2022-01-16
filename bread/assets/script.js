035a7d6e1cecf3a19be5c6c9062703f79625e612
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-01-15 23:30:10 -0300

    Create script.js

---
+++ bread/assets/script.js
@@ -0,0 +1,31 @@
+ var breadsong = new Audio('assets/bread.mp3');
+  var breadtwo = new Audio('assets/breadtwo.mp3');
+  var counter= 0;
+  var timer;
+  function bread() {
+  if(counter <= 7){
+    breadsong.play();
+    counter ++;
+  }else{
+    breadtwo.play();
+    counter = 0;
+    timer = 6;
+     timerset();
+  }
+  }
+  
+  function timerset(){
+    timer = timer - 1;
+    if(timer > 0){
+      document.getElementById('bread').style.display = 'none';
+    document.getElementById('title').style.display = 'none';
+    document.getElementById('timeout').style.display = 'block';
+    document.getElementById('timeout').innerHTML = 'You ate too much bread, you can eat again in ' + timer + ' seconds';
+    setTimeout(timerset, 1000);
+  }else{
+    document.getElementById('bread').style.display = 'block';
+    document.getElementById('title').style.display = 'block';
+    document.getElementById('timeout').style.display = 'none';
+  }
+  }
+  
