6bb0eca3e1411f12c3b01f3d7a2a0bef72ab8f97
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-01-17 13:32:30 -0300

    I can't fix it now without my PC
    I need a ~40mb file for the test, but i cant generate one using my phone, ill fix it later. The values are wrong for +10mbps connections, i can also try to change the image server too.

--- networkspeed/assets/script.js
+++ networkspeed/assets/script.js
@@ -1,7 +1,7 @@
   var teststart;
     var testend;
-    var testfile = 'https://www.dropbox.com/s/d5fjk0pyi5axqxx/image.jpg?dl=1';
-      var size = 56644075;
+    var testfile = 'https://www.dropbox.com/s/q3zu9l178uau3jm/acoolphotoitook.jpg?dl=1';
+      var size = 5819596;
       var image = new Image()
     
       
