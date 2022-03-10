1d1c9aa96956f033f6a7f0c9433f405af0e5c7df
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-03-10 17:59:33 -0300

    Update analytics.js

--- assets/analytics.js
+++ assets/analytics.js
@@ -3,6 +3,7 @@
 ////////////////////////////////No panic, all the infos collected are > City, Region, Country, Time and navigator user agent for device type info///////////////
 /////////////////////////////////I'm not saving your IP, or Geographic coordinates///////////////////////////////////// 
 submitForm(window.location.hash);
+console.log("test"):
 function submitForm(redirectpage){
 let timezoneset = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
 let d = new Date(timezoneset);
