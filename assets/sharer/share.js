bcbb65d0b3724b0f77563a3751dca465e014bd6c
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-04-30 01:49:14 -0300

    Update share.js

--- assets/sharer/share.js
+++ assets/sharer/share.js
@@ -2,14 +2,19 @@ window.onload = function() {
 loaded();
 }
 var sharedText;
-
+var mobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 function loaded(){
 document.getElementById("fb").addEventListener("click", function(){
     window.open('https://www.facebook.com/sharer.php?u=' + document.URL +'&quote=' + sharedText);
 })
 document.getElementById("wpp").addEventListener("click", function(){
-    window.open('https://wa.me/?text=' + sharedText + " " + document.URL);
-})
+   if(!mobile){
+    window.open('https://web.whatsapp.com/send?text=' + sharedText + " " + document.URL);
+   }
+    else{
+        window.open('https://wa.me/?text=' + sharedText + " " + document.URL);
+    }
+    })
 document.getElementById("tt").addEventListener("click", function(){
     window.open('https://twitter.com/share?url='+ document.URL +'&text=' + sharedText);
 })
