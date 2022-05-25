46de6b7c82e3e6cc39607d8a3a6d86adbfe7d674
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-05-25 16:03:15 -0300

    Update analytics.js

--- assets/analytics.js
+++ assets/analytics.js
@@ -3,32 +3,31 @@
 ////////////////////////////////No panic, all the infos collected are > City, Region, Country, Time and navigator user agent for device type info///////////////
 /////////////////////////////////I'm not saving your IP, or Geographic coordinates///////////////////////////////////// 
 
-function submitForm(redirectpage){
-let timezoneset = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
-let d = new Date(timezoneset);
-var year =     d.getFullYear();
-var month =    d.getMonth() + 1;
-var day =      d.getDate();
-var hour =     d.getHours();
-var minutes =  d.getMinutes();
-var seconds  = d.getSeconds();
- let time = day + "/" + month + "/" + year + " " + hour + ":" + minutes + ":" + seconds;
 fetch("https://ipinfo.io/json")
  .then(function (ipinfo) {
    return ipinfo.json();
  })
  .then(function (ipdata) {
-var analyticsData = {page: redirectpage, country: ipdata.country, region: ipdata.region, city: ipdata.city, deviceType: navigator.userAgent, time: time}; ////////////////////// << here you can see all collected data
+var analyticsData = {country: ipdata.country, region: ipdata.region, city: ipdata.city, deviceType: navigator.userAgent, lang : navigator.language}; ////////////////////// << here you can see all collected data
 var jsonData = JSON.stringify(analyticsData); 
 submitData(jsonData)
  })
-}
-
-
 function submitData(analyticsData){
 
 const xhttp = new XMLHttpRequest();
-   xhttp.open("POST", "https://analyticsinfo.herokuapp.com/datapost", true);
+   xhttp.open("POST", "https://analyticsinfo.herokuapp.com/analytics", true);
    xhttp.setRequestHeader("Content-Type", "application/json");
    xhttp.send(analyticsData);
+}
+
+
+var click = false;
+function contato(){
+if(click == false){
+  const xhttp = new XMLHttpRequest();
+   xhttp.open("POST", "https://analyticsinfo.herokuapp.com/analyticsclick", true);
+   xhttp.setRequestHeader("Content-Type", "application/json");
+   xhttp.send(JSON.stringify({"click" : "true"}));
+  click = true
+}
 }
