77f7c10afeb51e07294f972adaa75a581f023c86
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-05-25 17:24:56 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
-function changeLang(){
-    var currentLang = document.getElementById("lang").value;
-    if(currentLang == "EN"){
-        location.replace("/EN");
-    }else if(currentLang == "PT"){
-     location.replace("/PT");
-    }
-}
+$("#name").fadeIn(1000, ()=>{
+   $("#what").fadeIn("slow")})
+   $("#info").fadeIn(1000);
+   
+   var lang = "hid";
 
+   function showlang(){
+       if(lang == "hid"){
+       $("#selector").fadeIn(400);
+       lang = "nhid";
+       }else{
+        $("#selector").fadeOut(400);
+        lang = "hid";
+       }
+   }
+   function setlang(lang){
+       showlang();
+       if(lang == "en"){
+           location.replace("/EN");
+       }
+       if(lang == "pt"){
+           location.replace("/PT");
+       }
+   }
